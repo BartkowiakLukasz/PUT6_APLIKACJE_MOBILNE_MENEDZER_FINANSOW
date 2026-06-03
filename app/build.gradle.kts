17e13b8b90a74bc2d05c7c4fa17fd7f80f6c6ca5
BartkowiakLukasz <lukasz.bartkowiak@student.put.poznan.pl>
2026-06-03 12:30:41 +0200

    Update Kotlin to 2.1.10, KSP to 2.1.10-1.0.29, Hilt to 2.55, and migration of google-services plugin to version catalog (libs.versions.toml)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    id("com.google.gms.google-services")
+    alias(libs.plugins.google.services)
 }
 
 val localProperties = Properties()
